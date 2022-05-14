0e3ab858ad9418fc9f4f0c7dfac91532774dfd94
tanvir-ux <tanviralamshuvo@gmail.com>
2022-05-14 19:16:16 +0600

    Road completed and Car moving properly

--- car.js
+++ car.js
@@ -14,6 +14,10 @@ class Car {
     }
 
     update() {
+        this.#move();
+    }
+
+    #move(){
         if (this.controls.forward) {
             this.speed += this.acceleration; 
         }
@@ -35,11 +39,14 @@ class Car {
         if(Math.abs(this.speed) < this.friction) {
             this.speed = 0;
         }
-        if (this.controls.left) {
-            this.angle += 0.03;
-        }
-        if( this.controls.right) {
-            this.angle -= 0.03;
+        if(this.speed != 0) {
+            const flip = this.speed>0?1:-1;
+            if (this.controls.left) {
+                this.angle += 0.03*flip;
+            }
+            if( this.controls.right) {
+                this.angle -= 0.03*flip;
+            }
         }
         this.x -= Math.sin(this.angle) * this.speed;
         this.y -= Math.cos(this.angle) * this.speed;
