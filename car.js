858e24b446d745c95d776ce4b27a437d320989e5
tanvir-ux <tanviralamshuvo@gmail.com>
2022-05-16 20:02:28 +0600

    Neural Network and Network Mutation is done

--- car.js
+++ car.js
@@ -10,9 +10,11 @@ class Car {
         this.friction = 0.05;
         this.angle = 0;
         this.damaged = false;
+        this.useBrain = controlType == "AI";
 
         if(controlType != "DUMMY") {
             this.sensor = new Sensor(this);
+            this.brain  = new NeuralNetwork([this.sensor.rayCount, 6, 4]);// 6 hidden neurons, 4 outputs neurons
         }
         this.controls = new Controls(controlType);        
     }
@@ -23,8 +25,19 @@ class Car {
             this.polygon = this.#createPolygon();
             this.damaged = this.#assessDamage(roadBorders, traffic);
         }
-        if(this.sensor) {
+        if(this.sensor) {            
             this.sensor.update(roadBorders, traffic);
+             const offsets = this.sensor.readings.map(
+                 s=>s==null?0:1-s.offset
+             );
+             const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+             
+             if(this.useBrain) {
+                 this.controls.forward = outputs[0];
+                 this.controls.left    = outputs[1];
+                 this.controls.right   = outputs[2];
+                 this.controls.reverse = outputs[3];
+             }
         }
     }
 
@@ -100,7 +113,7 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx, color) {
+    draw(ctx, color, drawSensor = false) {
         if(this.damaged) {
             ctx.fillStyle = "gray";
         } else {
@@ -113,7 +126,7 @@ class Car {
         }
         ctx.fill();
 
-        if (this.sensor) {
+        if (this.sensor && drawSensor) {
             this.sensor.draw(ctx);
         }
     }
